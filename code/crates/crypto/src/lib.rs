f8172df54bd3ccf82d78e4d9a5d4d5b94fe01bc8
Romain Ruetschi <romain@informal.systems>
2024-09-25 17:03:06 +0200

    chore(code): Remove `malachite-crypto` crate (#422)

--- code/crates/crypto/src/lib.rs
+++
@@ -1,7 +0,0 @@
-// For coverage on nightly
-#![allow(unexpected_cfgs)]
-#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
-#![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
-
-#[cfg(feature = "ed25519")]
-pub mod ed25519;
