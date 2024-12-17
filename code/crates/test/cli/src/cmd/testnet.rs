973608d634f08fcc7fd063b3ecec8d10fa02fcd9
Romain Ruetschi <romain@informal.systems>
2024-12-17 10:56:40 +0100

    chore(code): Use TCP by default (#688)

--- code/crates/test/cli/src/cmd/testnet.rs
+++ code/crates/test/cli/src/cmd/testnet.rs
@@ -97,9 +97,9 @@ pub struct TestnetCmd {
 
     /// The transport protocol to use for P2P communication
     /// Possible values:
-    /// - "quic": QUIC (default)
-    /// - "tcp": TCP + Noise
-    #[clap(short, long, default_value = "quic", verbatim_doc_comment)]
+    /// - "tcp": TCP + Noise (default)
+    /// - "quic": QUIC
+    #[clap(short, long, default_value = "tcp", verbatim_doc_comment)]
     pub transport: TransportProtocol,
 }
 
