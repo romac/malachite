8e7f30b0e35680cc0130537389bb05680c4bf4e2
Romain Ruetschi <romain@informal.systems>
2025-02-14 14:03:16 +0100

    fix(code/app): Fix bug in test app and example app where proposal parts were not processed in order of sequence number (#851)

--- code/crates/test/app/src/streaming.rs
+++ code/crates/test/app/src/streaming.rs
@@ -46,10 +46,13 @@ impl<T> MinHeap<T> {
     }
 
     fn drain(&mut self) -> Vec<T> {
-        self.0
-            .drain()
-            .filter_map(|msg| msg.0.content.into_data())
-            .collect()
+        let mut vec = Vec::with_capacity(self.0.len());
+        while let Some(MinSeq(msg)) = self.0.pop() {
+            if let Some(data) = msg.content.into_data() {
+                vec.push(data);
+            }
+        }
+        vec
     }
 }
 
